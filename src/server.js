824bbd80d5923dabd5160910251b3ee3a7d414b2
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-06-16 21:52:53 -0300

    not found route

--- src/server.js
+++ src/server.js
@@ -10,8 +10,15 @@ app.use(express.json());
 
 app.use(routes);
 
-// catch all
+//not found
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+// catch all
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({ error: error.message });
